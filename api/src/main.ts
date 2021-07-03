7b83a0167325670a0e1b73b1cf1f7bfd1927c171
Yaroslav <lixometr@gmail.com>
2021-07-03 10:38:56 +0300

    fix api

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(helmet());
   app.enableCors();
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
