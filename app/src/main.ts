f1306c30c6f2599f6949cfa8abe0a444f5c5b91c
Yaroslav <lixometr@gmail.com>
2021-07-03 10:41:37 +0300

    fix

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,11 +3,12 @@ import App from "./App.vue";
 import Socketio from "@/plugins/socket";
 import router from "./router";
 import VueApexCharts from "vue3-apexcharts";
-import './assets/tailwind.css'
+import "./assets/tailwind.css";
 const app = createApp(App);
 
 app.use(Socketio, {
-  connection: "http://localhost:3000",
+  connection:
+    process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/api",
   options: {
     transports: ["websocket"],
   },
