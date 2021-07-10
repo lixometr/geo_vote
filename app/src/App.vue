4b400c2ddfb572b4f4dfc518596d95713b8997c8
Yaroslav <lixometr@gmail.com>
2021-07-10 12:55:55 +0300

    fix loading

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="app">
     <router-view></router-view>
+    <div
+      class="fixed top-0 left-0 right-0 bottom-0 bg-white z-50"
+      v-if="!isLoaded"
+    ></div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
 import Rellax from "rellax";
 export default defineComponent({
   name: "App",
   components: {},
   setup() {
+    const isLoaded = ref(false);
     window.addEventListener("load", () => {
       new Rellax(".rellax");
+      isLoaded.value = true;
     });
+    return { isLoaded };
   },
 });
 </script>
