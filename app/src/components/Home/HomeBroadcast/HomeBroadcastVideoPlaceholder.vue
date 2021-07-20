ace8bfd31e3d6789c91050755470aa47f5cebebb
Yaroslav <lixometr@gmail.com>
2021-07-20 18:56:02 +0300

    added mob placeholder image

--- app/src/components/Home/HomeBroadcast/HomeBroadcastVideoPlaceholder.vue
+++ app/src/components/Home/HomeBroadcast/HomeBroadcastVideoPlaceholder.vue
@@ -3,11 +3,26 @@
     <div
       class="absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-40"
     ></div>
-    <div class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      <home-broadcast-play @click="$emit('play')"/>
+    <div
+      class="
+        absolute
+        left-1/2
+        top-1/2
+        transform
+        -translate-x-1/2 -translate-y-1/2
+      "
+    >
+      <home-broadcast-play @click="$emit('play')" />
     </div>
     <div
-      class="text-lg text-white text-center absolute bottom-2 left-0 right-0 sm:text-sm sm:bottom-1"
+      class="
+        text-lg text-white text-center
+        absolute
+        bottom-2
+        left-0
+        right-0
+        sm:text-sm sm:bottom-1
+      "
     >
       Нажмите, чтобы воспроизвести
     </div>
@@ -26,5 +41,8 @@ export default defineComponent({
 .home-broadcast-video-placeholder {
   @apply bg-cover bg-center bg-no-repeat;
   background-image: url(~@/assets/img/broadcast_placeholder.jpg);
+  @screen sm {
+    background-image: url(~@/assets/img/broadcast_placeholder_mob.jpeg);
+  }
 }
 </style>
